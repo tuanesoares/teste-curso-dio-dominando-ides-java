2f5721c781402690485f67689254f719cc88a704
tuanesoares <tuane.teixeira1@hotmail.com>
2022-04-29 08:28:18 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,31 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		Gato gato = new Gato ();
+		Livros livros = new Livros();
+		
+		
+		System.out.println(gato);
+		System.out.println(livros);
+		
+		
+	}
+		
+	}
+
+class Livros {
+	private String nome;
+	private String npag;
+}
+		/*int a = 2;
+		int b = 3;
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
